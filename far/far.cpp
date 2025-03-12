dc0bee7585358c017d4369e0a78f3d139cd9b7cf
Shmuel Zeigerman <solomuz0@gmail.com>
2025-03-12 16:09:00 +0200

    Refactoring

--- far/far.cpp
+++ far/far.cpp
@@ -6,7 +6,7 @@ typedef void (*PLUGFUNC)();
 
 int main(int argc, const char* argv[])
 {
-  auto handle = dlopen("./Plugins/luafar/myplug/plug/myplug.far-plug-wide", RTLD_LAZY|RTLD_GLOBAL);
+  void *handle = dlopen("./Plugins/mylib/myplug/plug/myplug.far-plug-wide", RTLD_LAZY|RTLD_GLOBAL);
   if (handle) {
     PLUGFUNC plugfunc = (PLUGFUNC) dlsym(handle, "plugfunc");
     if (plugfunc)

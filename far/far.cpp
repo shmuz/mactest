1cdc9a9a7f69aec6ab30add4061d9dcb146aaedf
Shmuel Zeigerman <solomuz0@gmail.com>
2025-03-20 16:41:25 +0200

    Try to dlopen luajit

--- far/far.cpp
+++ far/far.cpp
@@ -16,6 +16,19 @@ int main(int argc, const char* argv[])
   }
   else
     printf("no handle\n");
+
+#ifdef __APPLE__
+	const char *lib = "libluajit-5.1.dylib";
+#else
+	const char *lib = "libluajit-5.1.so";
+#endif
+
+	void *handle2 = dlopen(lib, RTLD_LAZY|RTLD_GLOBAL);
+  if (handle2)
+		printf("LuaJIT was found\n");
+	else
+		printf("LuaJIT was NOT found\n");
+
   return 0;
 }
 

3bb5189f9dd0b993b76f265b778c7854e3a066d6
Shmuel Zeigerman <solomuz0@gmail.com>
2025-03-21 22:47:35 +0200

    Refactoring

--- myplug/src/myplug.lua
+++ myplug/src/myplug.lua
@@ -1,2 +1,11 @@
-print("from myplug.lua")
+-- plugin
+
+local fp = io.popen("env")
+if fp then
+  print("Environment:")
+  print( fp:read("*all") )
+  fp:close()
+else
+  print("could not popen'env'")
+end
 

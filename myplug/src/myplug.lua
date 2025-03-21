bf891fac1ed6a2caee1e76236046178a9d5b6976
Shmuel Zeigerman <solomuz0@gmail.com>
2025-03-22 00:18:42 +0200

    Refactoring

--- myplug/src/myplug.lua
+++ myplug/src/myplug.lua
@@ -1,11 +1,16 @@
 -- plugin
 
-local fp = io.popen("env")
-if fp then
-  print("Environment:")
-  print( fp:read("*all") )
-  fp:close()
-else
-  print("could not popen'env'")
-end
+-- local fp = io.popen("env")
+-- if fp then
+--   print("Environment:")
+--   print( fp:read("*all") )
+--   fp:close()
+-- else
+--   print("could not popen'env'")
+-- end
 
+local nm = "/tmp/far-script1.lua"
+local fp = assert( io.open(nm, "w") )
+fp:write("print'Hello World'")
+fp:close()
+dofile(nm)
